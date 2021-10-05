3dbc78b4b455362f850fcfd925b7f6bbf14b04c7
deborah <deborahregs@gmail.com>
2021-10-05 19:05:23 -0300

    Terceiro exercicio da lista 1.

---
+++ src/deborah/dbc/Exercicio3.java
@@ -0,0 +1,21 @@
+package deborah.dbc;
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o valor total consumido: ");
+        float valorConsumido = scanner.nextFloat();
+        scanner.nextLine();
+        System.out.print("Digite o valor pago pelo cliente: ");
+        float valorPago = scanner.nextFloat();
+        scanner.nextLine();
+        if (valorPago < valorConsumido) {
+            System.out.println("O valor pago deve ser maior ou igual ao valor consumido.");
+        }
+        else {
+            float troco = valorPago - valorConsumido;
+            System.out.println("O troco deve ser: " + troco);
+        }
+    }
+}

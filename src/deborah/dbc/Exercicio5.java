e722839c38787ca828b07edfd45d6b09231349ad
deborah <deborahregs@gmail.com>
2021-10-06 08:28:36 -0300

    Sexto exercicio da lista 1.

--- src/deborah/dbc/Exercicio5.java
+++ src/deborah/dbc/Exercicio5.java
@@ -7,11 +7,11 @@ public class Exercicio5 {
         System.out.print("Digite o valor pago por hora trabalhada: ");
         float valorDaHora = scanner.nextFloat();
         scanner.nextLine();
-        System.out.print("Digite o número de horas normais trabalhadas: ");
+        System.out.print("Digite o número de horas inteiras normais trabalhadas: ");
         int numeroDeHoras = scanner.nextInt();
         scanner.nextLine();
-        System.out.print("Digite o número horas extras trabalhadas: ");
-        int numeroHorasExtras = scanner.nextInt();
+        System.out.print("Digite o número horas inteiras extras trabalhadas: ");
+        float numeroHorasExtras = scanner.nextFloat();
         double salarioMensal = valorDaHora*numeroDeHoras + valorDaHora*numeroHorasExtras*0.5;
         System.out.println(String.format("O Salário do funcionário é R$%.2f: ",salarioMensal));
     }

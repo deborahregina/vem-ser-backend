3c602bd8f19564ee7ce49e313737f4f40e03a428
deborah <deborahregs@gmail.com>
2021-12-01 14:14:14 -0300

    consumer

--- modulo6- TÓPICOS ESPECIAIS/chatkafka/chatkafka/src/main/java/com/dbc/chatkafka/service/ConsumerService.java
+++ modulo6- TÓPICOS ESPECIAIS/chatkafka/chatkafka/src/main/java/com/dbc/chatkafka/service/ConsumerService.java
@@ -12,12 +12,17 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Formatter;
+
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class ConsumerService {
     private final KafkaTemplate<String, String> stringKafkaTemplate;
     private final ObjectMapper objectMapper;
+
+
 
     @KafkaListener(
             topics = "${kafka.topic.string}",
@@ -28,7 +33,22 @@ public class ConsumerService {
                            @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
                            @Header(KafkaHeaders.OFFSET) Long offset) throws JsonProcessingException {
         MensagemDTO mensagemDTO = objectMapper.readValue(mensagem, MensagemDTO.class);
-        log.info("MENSAGEM LIDA: '{}', CHAVE: '{}', OFFSET: '{}'", mensagemDTO, key, offset);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss");
+        log.info("DATA CRIACAO: {} [{}]: {}",formatter.format(mensagemDTO.getDataCriacao()) ,mensagemDTO.getUsuario(), mensagemDTO.getMensagem());
+
+    }
+
+    @KafkaListener(
+            topics = "${kafka.topic.reservado}",
+            groupId = "${kafka.group-id}",
+            containerFactory = "listenerContainerFactoryEarliest"
+    )
+    public void consume(@Payload String mensagem,
+                           @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
+                           @Header(KafkaHeaders.OFFSET) Long offset) throws JsonProcessingException {
+        MensagemDTO mensagemDTO = objectMapper.readValue(mensagem, MensagemDTO.class);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss");
+        log.info("DATA CRIACAO: {} [{}](privado): {}",formatter.format(mensagemDTO.getDataCriacao()), mensagemDTO.getUsuario(), mensagemDTO.getMensagem());
 
     }
 }
